57a2aff63c550053c4d25b4d8a9b8cfa4f2d3f5c
Adewale Orotayo <didymus7007@gmail.com>
2020-11-23 11:32:14 +0100

    Fetching a single resource by setting the id of the resource 2. Id parameter in the route of the request is accessed through the request object 3. The id is found using the array find method

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 });
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find(note => note.id === id);
+  response.json(note);
 });
 
 const PORT = 3001;
