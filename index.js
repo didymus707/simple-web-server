a75eab41354be29dc3b2ea41e63c029aabdbafb7
Adewale Orotayo <didymus7007@gmail.com>
2020-11-24 13:17:51 +0100

    Added a function to dynamically generate id serially preventing empty object from being addd in the post method

--- index.js
+++ index.js
@@ -31,13 +31,6 @@ let notes = [
   }
 ]
 
-app.post('/api/notes', (request, response) => {
-  const note = request.body;
-  console.log(note);
-
-  response.json(note);
-})
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 });
@@ -57,6 +50,26 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+  return maxId + 1;
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  notes = notes.concat(note);
+  response.json(note);
 });
 
 const PORT = 3001;
