8cca65328b68c599a2503baeea49946d76ab7b4e
Adewale Orotayo <didymus7007@gmail.com>
2020-11-24 11:52:11 +0100

    Readded the index route for notes added a delete method for a resource

--- index.js
+++ index.js
@@ -26,10 +26,21 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 });
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find(note => note.id === id);
   note ? response.json(note) : response.status(404).end();
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
